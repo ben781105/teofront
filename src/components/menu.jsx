8959344f306fcbca11e49433d600146897bbea0a
ben781105 <159682984+ben781105@users.noreply.github.com>
2025-03-27 17:53:35 +0300

    Update menu.jsx

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -2,6 +2,7 @@ import "../styles/menu.css";
 import SkeletonGrid from "./skeletongrid";
 import ErrorComponent from "./ErrorComponent"; // Import the error component
 import { Link } from "react-router-dom";
+import imageUrl from '../imageurl'
 function Menu({ cakes, loading, error }) {
   return (
     <div className="menu" id="menu">
@@ -15,7 +16,7 @@ function Menu({ cakes, loading, error }) {
         <div className="menu-grid">
           {cakes.map((cake) => (
             <Link to={`/cake-detail/${cake.slug}`} key={cake.id}><div className="menu-item" style={{color:'inherit'}}>
-              <div className="menu-image"><img src={`http://localhost:8000${cake.image}`} alt={cake.name}/></div>
+              <div className="menu-image"><img src={`${imageUrl}${cake.image}`} alt={cake.name}/></div>
               <h3 style={{textDecoration:'none',color:'inherit'}}>{cake.name}</h3>
               <p>${(cake.price).toLocaleString()}</p>
               
