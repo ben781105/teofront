60ff0731247192ad63d615f933bdcfaec1672bc7
Ssewabuga Benard <159682984+ben781105@users.noreply.github.com>
2025-04-02 13:12:19 +0300

    Update orderhistory.jsx

--- src/components/orderhistory.jsx
+++ src/components/orderhistory.jsx
@@ -1,20 +1,20 @@
 import '../styles/orderhistory.css'
 import imageUrl from '../imageurl'
-function Orderhistory({orders}) {
+function Orderhistory() {
   return (
     <div className="order-history-container">
         <h3>Order history</h3>
-       {orders.map((order)=>
-       <div className="history-wrapper" key={order.id}>
+       
+       <div className="history-wrapper">
        <div className="product-info">
         <div className='order-image'><img src={`${imageUrl}${order.cake.image}`} alt="item image" /></div>
-           <h2>item name:  {order.cake.name}</h2>
-           <p>Order date:  {order.order_date}</p>
-           <p>Order ID:   {order.order_id}</p>
+           <h2>item name: Red Velvet </h2>
+           <p>Order date: 01/April/2025</p>
+           <p>Order ID: Wx5jvJKWHJyy </p>
        </div>
-       <p>Quantity: {order.quantity}</p>
-       <span>Price :{order.cake.price}</span>
-</div>)}
+       <p>Quantity: 1 </p>
+       <span>Price : $200</span>
+</div>
     </div>
   )
 }
